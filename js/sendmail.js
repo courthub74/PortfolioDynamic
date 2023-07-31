84ffed4395b511a127b0d5022066822c6c0ba42e
Courdevelops <courdevelops@gmail.com>
2023-07-31 16:41:12 -0400

    email works

---
+++ js/sendmail.js
@@ -0,0 +1,27 @@
+// Index JS
+// alert("Hello World!");
+// SendMail Function
+function sendMail() {
+    console.log("Hello World!");
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+
+// IDs
+const serviceID = "service_mq97z9o";
+const templateID = "template_3zsnggm";
+
+// email JS send function
+emailjs.send(serviceID, templateID, params)
+.then(
+    (res) => {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully");
+    })
+    .catch((err) => console.log(err));
+}
